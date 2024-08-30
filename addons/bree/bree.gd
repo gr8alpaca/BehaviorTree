8f1b6da57f7a03d261ce1fc00546b1e5fb354ffa
Hugo <148060520+gr8alpaca@users.noreply.github.com>
2024-08-30 09:41:28 -0400

    Init repo

--- addons/bree/bree.gd
+++ addons/bree/bree.gd
@@ -3,12 +3,13 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	
 	pass
 
 
 func _exit_tree() -> void:
 	pass
-
-func force_multi_window_mode() -> void:
-	pass
+#
+#func force_multi_window_mode() -> void:
+	#if EditorInterface.is_multi_window_enabled(): return
+	#var edit_settings: EditorSettings = EditorInterface.get_editor_settings()
+	#edit_settings.set_setting()
